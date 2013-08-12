f74925a25d8445647be709e1fdcfb68724a5ae0c
yveso <yves.oehlschlaeger+gh@googlemail.com>
2013-08-12 22:39:02 +0200

    Game.js added

---
+++ js/Game.js
@@ -0,0 +1,33 @@
+function Game(framesPerSecond) {
+  var fps = framesPerSecond;
+  var intervalId;
+  var isRunning = false;
+
+  this.start = function () {
+    isRunning = true;
+    intervalId = setInterval(function () {
+      update();
+      draw();
+    }, 1000 / fps);
+    console.log("game loop started")
+  }
+
+  this.stop = function () {
+    isRunning = false;
+    clearInterval(intervalId);
+    console.log("game loop ended");
+  }
+
+  this.getIsRunning = function () {
+    return isRunning;
+  }
+
+  var update = function () {
+    console.log("updating");
+  }
+
+  var draw = function () {
+    console.log("drawing");
+  }
+
+}
